674bb05e421df11a5a4aaecbff4025ab26b78687
JoaoKrapp <joaokrappcurso@gmail.com>
2022-01-16 12:22:24 -0300

    Tranferir, Editar, Novas Receitas, etc..

--- src/profiles/static/profiles/profiles_my.js
+++ src/profiles/static/profiles/profiles_my.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
         url: '/profiles/contas-json',
         success: function(response){
 
-            console.log(response)
           
             const feed = response.contas
 
